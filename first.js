98bdf9389efba995b5590e86e11237c292a77aef
dev-aranya <oviaranya025@gmail.com>
2025-12-16 19:30:53 +0600

    Add files via upload

---
+++ first.js
@@ -0,0 +1,91 @@
+// let score=prompt("Enter your marks ")
+// let grade;
+
+// if(score>=80 && score<=100){
+//     console.log("Grade A");
+// }
+
+// else if(score>=70 && score<=80){
+//     console.log("Grade B");
+// }
+// else if(score>=60 && score<=70){
+//     console.log("Grade B-");
+// }
+// else if(score>=50 && score<=60){
+//     console.log("Grade c");
+// }
+// else if(score>=40 && score<=50){
+//     console.log("Grade c-");
+// }
+// else if(score>=35 && score<=40){
+//     console.log("Grade D");
+// }
+// else if(score>=30 && score<=350){
+//     console.log("Grade D-");
+// }
+
+// for(let count=1;count<10;count++){
+//     console.log("Ovi debnath");
+// }
+
+// let n=prompt("Enter limit");
+// let sum=0;
+// for(let i =1;i<=n;i++){
+//     sum+=i;
+// }
+// console.log("Sum =",sum);
+
+// string & array -> of
+// let str="JavaScript";
+// let size=0;
+// for(let i of str){
+//     console.log("i= ",i);
+//     size++;
+// }
+// console.log("Size = ",size);
+
+//object
+// let student ={
+//  name:ondevicemotion,
+//  age:23,
+//  cgpa:3.8,
+//  ispass:true
+// };
+// for(let i in student){
+//     console.log(i);
+// }
+
+
+//practice 01
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+//practice 02
+// let guessnum = 20;
+// let usernum = prompt("  Guess the game number = ");
+// while (guessnum != usernum) {
+//     usernum = prompt(" You  guess wrong number. please guess again = ");
+// }
+// console.log("congratualtions, you entired the right number");
+
+// let n= prompt("Enter number : ");
+// let arr=[];
+// for(let i=1;i<=n;i++){
+//     arr[i-1]=i;
+// }
+// console.log(arr);
+
+// let sum= arr.reduce((res,cur)=>{
+//     return res+cur;
+// });
+// console.log("sum =",sum);
+
+// let fact=arr.reduce((res,cur)=>{
+//     return res*cur;
+// });
+// console.log("Factorial = ", fact);
+
